273dd5604158b5c444ebd54dc9fb85b227f82a65
kolibris111 <palubinskaitrasele1@gmail.com>
2024-03-21 15:42:24 +0200

    Uzduotys

---
+++ src/Book.java
@@ -0,0 +1,47 @@
+public class Book {
+    public String title;
+    public int pages;
+    public int releaseYear;
+
+    public Book() {
+    }
+    // Sukurti setterius//
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+    //Sukurti getterius//
+    public String getTitle() {
+        return this.title;
+    }
+    public int getPages() {
+        return this.pages;
+    }
+    public int getReleaseYear() {
+        return this.releaseYear;
+    }
+    public Book() {
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Book book1 = new Book();
+        Book book2 = new Book();
+        Book book3 = new Book();
+
+        Book book1 = new Book();
+        book1.setTitle("Kaktusas ar pienė");
+        book1.setPages(270);
+        book1.setReleaseYear(2021);
+
+        book2.setTitle("Tiems, kurie neskaito");
+        book2.setPages(240);
+        book2.setReleaseYear(2024);
+
+        book3.setTitle("Nebaigti reikalai");
+        book3.setPages(320);
+        book3.setReleaseYear(2024);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
